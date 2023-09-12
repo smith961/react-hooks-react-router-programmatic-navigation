fdb4c40e789d7a914a548588c36d9a1fe1051a74
unknown <smithbabawande@gmail.com>
2023-09-12 17:29:49 +0100

    Completed Assignment

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,24 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 
+function NavBar({ onLogout }) {
+  const history = useHistory();
+
+  function handleClick() {
+    // logout the user
+    onLogout();
+    // then navigate them to the login page
+    history.push("/login");
+  }
+
+  return (
+    <nav>
+      <button onClick={handleClick}>Logout</button>
+    </nav>
+  );
+}
+
+
 function Login({ setIsLoggedIn }) {
   const history = useHistory();
   const [formData, setFormData] = useState({
